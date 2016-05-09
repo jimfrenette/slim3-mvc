cbb84a54f24b01cda5f66c517ee135c40e2bb490
U-VGN-FW290\admin <jimfrenette@yahoo.com>
2016-05-08 22:38:08 -0400

    rm dead code

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,7 @@
 <?php
 
 $app->get('/', ['\App\Controllers\HomeController', 'index']);
-//$app->get('/', 'HomeController:index')->setName('home');
 
-//$app->get('/place', 'PlaceController:index')->setName('place');;
 $app->get('/place', ['\App\Controllers\PlaceController', 'index']);
 
-//$app->get('/place/{id}', 'PlaceController:getPlace');
 $app->get('/place/{id}', ['\App\Controllers\PlaceController', 'getPlace']);
